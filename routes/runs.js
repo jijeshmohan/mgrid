7d99f79e544cd42fa3917fcf29035d2c864a097a
Jijesh Mohan <jijeshmohan@gmail.com>
2013-12-12 22:38:27 +0530

    display run in descending order of time

--- routes/runs.js
+++ routes/runs.js
@@ -2,7 +2,7 @@ var form = require('express-form'),
     field = form.field;
 
 exports.list = function(req, res) {
-    models.Run.findAll().success(function(runs) {
+    models.Run.findAll({order: 'createdAt DESC'}).success(function(runs) {
         res.render('runs/index', {
             runs: runs,
             menu: 'runs'
