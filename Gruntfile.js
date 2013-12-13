e8d44d328daa3a9fc890290545e95dcbfd687999
Jijesh Mohan <jijeshmohan@gmail.com>
2013-12-13 20:10:02 +0530

    added grunt task for sequelize

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,14 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    sequelize: {
+      options: {
+        migrationsPath: __dirname + '/migrations',
+        database: "database_dev",
+        dialect: "sqlite",
+        storage: "db/database_dev.sqlite"
+      }
+    },
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
      all: ['Gruntfile.js', 'model/**/*.js', 'routes/**/*.js']
@@ -10,7 +18,7 @@ module.exports = function(grunt) {
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-jshint'); 
-
+  grunt.loadNpmTasks('grunt-sequelize');
   // task(s).
   grunt.registerTask('default', ['jshint']);
 
