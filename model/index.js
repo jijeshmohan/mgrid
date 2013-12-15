4fdde35919536bff9c99aededed52e433b1c4173
Jijesh Mohan <jijeshmohan@gmail.com>
2013-12-15 14:21:03 +0530

    added run items models

--- model/index.js
+++ model/index.js
@@ -1,6 +1,15 @@
 module.exports = function(sequelize) {
-    return {
+    var models = {
         Device: sequelize.import(__dirname + '/device'),
         Run: sequelize.import(__dirname + '/run'),
+  		RunItem: sequelize.import(__dirname + '/runitem')
     };
+
+    // Associations
+	models.Run.hasMany(models.RunItem);
+	models.Device.hasMany(models.RunItem);
+	models.RunItem.belongsTo(models.Run);
+	models.RunItem.belongsTo(models.Device);
+
+    return models;
 };
---
+++ model/runitem.js
@@ -0,0 +1,11 @@
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('runitem',{
+    comments: {
+       type: DataTypes.STRING(2000)
+    },
+    status: {
+       type:   DataTypes.ENUM,
+       values: ['Passed', 'Failed', 'Error', 'Running']
+    }
+  });
+};
