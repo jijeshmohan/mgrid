70f379bcd030bec9a9df0c1c92843c9b992e3b39
Jijesh Mohan <jijeshmohan@gmail.com>
2013-12-25 21:43:32 +0530

    Scenario model

--- model/index.js
+++ model/index.js
@@ -2,14 +2,18 @@ module.exports = function(sequelize) {
     var models = {
         Device: sequelize.import(__dirname + '/device'),
         Run: sequelize.import(__dirname + '/run'),
-  		RunItem: sequelize.import(__dirname + '/runitem')
+        RunItem: sequelize.import(__dirname + '/runitem'),
+  		Scenario: sequelize.import(__dirname + '/scenario')
     };
 
     // Associations
 	models.Run.hasMany(models.RunItem);
-	models.Device.hasMany(models.RunItem);
+    models.Device.hasMany(models.RunItem);
+	models.RunItem.hasMany(models.Scenario);
+    
 	models.RunItem.belongsTo(models.Run);
-	models.RunItem.belongsTo(models.Device);
+    models.RunItem.belongsTo(models.Device);
+	models.Scenario.belongsTo(models.RunItem);
 
     return models;
 };
---
+++ model/scenario.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('scenario',{
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    comments: {
+       type: DataTypes.STRING(2000)
+    },
+    status: {
+       type:   DataTypes.ENUM,
+       values: ['Passed', 'Failed', 'Skipped']
+    },
+    feature: {
+         type: DataTypes.STRING(2000)
+    },
+  },{
+    classMethods: {
+      
+    },
+    instanceMethods: {
+    }
+  });
+};
