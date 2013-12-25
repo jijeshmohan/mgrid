a0de45f84f7c10af48a51c4980dde1cc687b34fa
Jijesh Mohan <jijeshmohan@gmail.com>
2013-12-25 17:55:32 +0530

    added migration for scenarios

---
+++ migrations/20131225175217-add_scenarios.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    migration.createTable(
+    'scenarios',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      runitemId: {
+        type: DataTypes.INTEGER
+      },
+      feature: {
+         type: DataTypes.STRING(2000)
+      },
+      status: {
+         type:   DataTypes.ENUM,
+         values: ['Passed', 'Failed', 'Skipped']
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+      }
+    });
+    done()
+  },
+  down: function(migration, DataTypes, done) {
+    migration.dropTable('scenario');
+    done()
+  }
+}
