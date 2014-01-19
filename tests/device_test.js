c754ff49ae148bdbf76ace02fefc2fffb1f2b4d5
Jijesh Mohan <jijeshmohan@gmail.com>
2014-01-19 15:55:40 +0530

    added more test

--- tests/device_test.js
+++ tests/device_test.js
@@ -26,6 +26,7 @@ describe('Device', function() {
        
     });
 
+
 	 it('should have required properties', function(done){
 	 		var device =  models.Device.build(fakeDevice1);
             device.save().success(function(){
@@ -36,13 +37,35 @@ describe('Device', function() {
 			  done(error);
 			});
      });
+	 describe('class method',function(){
+	 	beforeEach(function(done){
+	 			models.Device.bulkCreate([fakeDevice1,fakeDevice2]).success(function(devices) { 
+	 				done();
+	 			}).error(done);
+	 	});
+	 	it('get all available devices',function(done){
+	 		models.Device.availableDevices().success(function(devices){
+	 			devices.length.should.equal(1);	
+	 			devices[0].should.have.property('name', 'Test device2');
+	 			done();
+	 		}).error(done);
+	 	});
 
-     it('isAvailable',function(done){
-     	models.Device.bulkCreate([fakeDevice1,fakeDevice2]).success(function(devices) { 
-     		devices.length.should.equal(2);
-     		devices[0].isAvailable().should.be.false;
-     		devices[1].isAvailable().should.be.true;
-     		done();
-     	}).error(done);
-     });
+	 	it('get all available devices count',function(done){
+	 		models.Device.availableCount().success(function(count){
+	 			count.should.equal(1);	
+	 			done();
+	 		}).error(done);
+	 	});
+	 });
+	 describe('instance method',function(){
+	     it('isAvailable',function(done){
+	     	models.Device.bulkCreate([fakeDevice1,fakeDevice2]).success(function(devices) { 
+	     		devices.length.should.equal(2);
+	     		devices[0].isAvailable().should.be.false;
+	     		devices[1].isAvailable().should.be.true;
+	     		done();
+	     	}).error(done);
+	     });
+	 });
 });
